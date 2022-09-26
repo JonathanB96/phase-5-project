64a4a78721328251a1c263cbfdb0fa14ab6e0dd1
jonathan bola <bolajonathan@yahoo.com>
2022-09-26 10:42:20 -0500

    login & logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+
+      #login
+      def create        
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password]) 
+            session[:user_id]||= user.id
+            render json: user
+        else
+            render json: {errors: ["not authorized"]}, status: :unauthorized
+        end
+     end
+    #logout
+    def destroy
+        user = User.find(session[:user_id])
+        if user
+            session.delete :user_id
+            head :no_content
+        else
+            render json: {errors: ["not authorized"]}, status: :unauthorized
+        end
+    end
 end
