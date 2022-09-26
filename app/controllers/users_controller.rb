1dccc2d7b34be3ef91b3fa094a3b28a52d85e289
jonathan bola <bolajonathan@yahoo.com>
2022-09-26 10:47:09 -0500

    user signup & auto-login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+    def create
+        user = User.create(user_params)
+        session[:user_id] ||= user.id 
+        if user.valid?
+          render json: user, status: :created
+        else
+          render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+     # Auto-login
+    def show
+        user = User.find(session[:user_id])
+            if user
+                render json: user
+            else
+                render json: {error: "not authorized"}, status: :unauthorized
+            end           
+    end    
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
 end
