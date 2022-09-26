459e43d40fb4ec8d608137d6660f692ec72791d6
jonathan bola <bolajonathan@yahoo.com>
2022-09-26 09:59:15 -0500

    creating routes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,5 @@
 class Recipe < ApplicationRecord
+    validates :name, :steps, :image_url, presence: true
     belongs_to :user
     belongs_to :category
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    validates :username, presence: true
     has_many :recipes
     has_many :categories, through: :recipes
     has_one :profile
