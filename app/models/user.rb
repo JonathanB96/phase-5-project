5b2c481ce8f5f9752f6c5a6a092fee14cd976764
jonathan bola <bolajonathan@yahoo.com>
2022-09-26 11:42:38 -0500

    migration & seed data

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
     has_many :recipes
     has_many :categories, through: :recipes
     has_one :profile
+    has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 Rails.application.routes.draw do
 
   resources :recipes
+  resources :categories, only: [:index]
   post '/signup', to: 'users#create'
   get '/me', to: 'users#show'
   post '/login', to: 'sessions#create'
