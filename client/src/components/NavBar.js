5491fdee9d3497637d7aba2bf90f2dc7932783ba
jonathan bola <bolajonathan@yahoo.com>
2022-09-28 11:29:32 -0500

    nav bar component

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,7 +1,88 @@
 import React from 'react'
+import "../styles/NavBar.css"
+import { NavLink } from "react-router-dom";
+// import { useHistory } from 'react-router';
 
-export default function NavBar() {
-  return (
-    <div>NavBar</div>
-  )
+const linkStyles = {
+    display: "inline-block",
+    width: "50px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "blue",
+    textDecoration: "none",
+    color: "white",
+  };
+
+export default function Header() {
+
+    function updatemenu() {
+        if (document.getElementById('responsive-menu').checked == true) {
+          document.getElementById('menu').style.borderBottomRightRadius = '0';
+          document.getElementById('menu').style.borderBottomLeftRadius = '0';
+        }else{
+          document.getElementById('menu').style.borderRadius = '5px';
+        }
+      }
+    // const history = useHistory()
+    function handleClick(){
+        // history.push('/')
+      }
+        
+      
+  return <>
+  <nav id='menu'>
+  <input type='checkbox' id='responsive-menu' onClick={updatemenu}/><label></label>
+  <ul>
+    <li> <NavLink
+        to="/"
+        exact
+        activeStyle={{
+          background: "#000",
+        }}
+      >
+        Home
+      </NavLink>
+      </li>
+    <li> <NavLink 
+        to="/signup"
+        exact
+        activeStyle={{
+          background: "#000",
+        }}
+      >
+        Sign Up
+      </NavLink></li>
+    <li><NavLink
+        to="/login"
+        exact
+        activeStyle={{
+          background: "#000",
+        }}
+      >
+        Login
+      </NavLink></li>
+    <li> <NavLink
+        to="/addrecipe"
+        exact
+        activeStyle={{
+          background: "#000",
+        }}
+      >
+        Add recipe
+      </NavLink></li>
+  </ul>
+
+    
+     
+      
+     
+
+
+
+
+  <div style={{float :"right",
+marginRight: "10%", }}><h1 style={{color:"#fff", cursor : "pointer"}}  onClick={handleClick}>//Recipe Viewer</h1></div>
+ </nav>
+  </>
 }
+
